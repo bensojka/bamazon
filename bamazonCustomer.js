7b3256970e646d7daebf749430040f7fe3d382cf
Ben Sojka <bensojka@gmail.com>
2017-09-16 09:27:06 -0500

    9/16

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var key = "ITEM ID | PRODUCT NAME | DEPARTMENT NAME | PRICE | STOCK QUANTITY"
 
 // connection information for the sql database
 
@@ -17,102 +18,118 @@ var connection = mysql.createConnection({
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-    if (err) throw err;
-    // run the readProducts function
-    readProducts();
+  if (err) throw err;
+  // run the readProducts function
+  uno();
 });
 
-function readProducts() {
-  console.log("ITEM ID | PRODUCT NAME | DEPARTMENT NAME | PRICE | STOCK QUANTITY")
-  connection.query("SELECT * FROM products", function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " remaining");
+function uno() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      choices: ['VIEW INVENTORY AND PRICE LIST', 'MAKE A PURCHASE'],
+      name: 'action',
+      message: 'Select an option to continue:'
     }
-    console.log("-----------------------------------");
-    whatProduct();
-  });
+  ]).then(function(response) {
+    if ( response.action === 'VIEW INVENTORY AND PRICE LIST' )
+      readProducts();
+    else if ( response.action === 'MAKE A PURCHASE' ) 
+      whatProduct();
+    });
+  }
+
+function readProducts() {
+console.log(key);
+connection.query("SELECT * FROM products", function(err, res) {
+  for (var i = 0; i < res.length; i++) {
+    console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " remaining");
+  }
+  console.log("-----------------------------------");
+  nextMove();
+});
 }
 
 function whatProduct() {
-    // query the database for all items being sold
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // once you have the items, prompt the user for which they'd like to buy
-      inquirer
-        .prompt([
-          {
-            name: "choice",
-            type: "rawlist",
-            choices: function() {
-              var choiceArray = [];
-              for (var i = 0; i < res.length; i++) {
-                choiceArray.push(res[i].product_name);
-              }
-              return choiceArray;
-            },
-            message: "What product would you like to buy? \n  Select by typing the item id number:"
-          },
-          {
-            name: "quantity",
-            type: "input",
-            message: "How many would you like to purchase?"
-          }
-        ])
-        .then(function(answer) {
-          // get the information of the chosen item
-          var chosenItem;
-          for (var i = 0; i < res.length; i++) {
-            if (res[i].product_name === answer.choice) {
-              chosenItem = res[i];
+  // query the database for all items being sold
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to buy
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "list",
+          pageSize: res.length + 1,
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < res.length; i++) {
+              choiceArray.push(res[i].product_name);
             }
+            return choiceArray;
+          },
+          message: "SELECT AN ITEM TO PURCHASE:"
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How many would you like to purchase?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].product_name === answer.choice) {
+            chosenItem = res[i];
           }
-  
-          // determine if there is enough inventory
-          if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
-            // there is enough inventory, so update db, let the user know, and start over
-            connection.query(
-              "UPDATE products SET ? WHERE ?",
-              [
-                {
-                  stock_quantity: chosenItem.stock_quantity - answer.quantity
-                },
-                {
-                  item_id: chosenItem.item_id
-                }
-              ],
-              function(err) {
-                if (err) throw err;
-                console.log("Your purchase was successful! You were charged $" + (chosenItem.price * answer.quantity) + ".");
-                nextMove();
+        }
+
+        // determine if there is enough inventory
+        if (chosenItem.stock_quantity >= parseInt(answer.quantity)) {
+          // there is enough inventory, so update db, let the user know, and start over
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: chosenItem.stock_quantity - answer.quantity
+              },
+              {
+                item_id: chosenItem.item_id
               }
-            );
-          }
-          else {
-            // not enough product, so apologize and start over
-            console.log("You requested to purchase more product than we have in stock. There are only " + chosenItem.stock_quantity + " items remaining...");
-            nextMove();
-          }
-        });
-    });
+            ],
+            function(err) {
+              if (err) throw err;
+              console.log("Your purchase was successful! You were charged $" + (chosenItem.price * answer.quantity) + ".");
+              nextMove();
+            }
+          );
+        }
+        else {
+          // not enough product, so apologize and start over
+          console.log("You requested to purchase more product than we have in stock. There are only " + chosenItem.stock_quantity + " items remaining...");
+          nextMove();
+        }
+      });
+  });
 }
 
 function nextMove() {
-  inquirer
-    .prompt({
-      name:"stayOrLeave",
-      type: "rawlist",
-      message: "Would you like to [CONTINUE SHOPPING] or [LEAVE STORE]?",
-      choices: ["CONTINUE SHOPPING", "LEAVE STORE"]
-    })
-    .then(function(answer) {
-      if (answer.stayOrLeave.toUpperCase() === "CONTINUE SHOPPING") {
-        readProducts();
-      }
-      else {
-        console.log("Thanks for shopping with Bamazon!");
-        process.exit();
-      }
-    });
+inquirer
+  .prompt({
+    name:"stayOrLeave",
+    type: "list",
+    message: "Would you like to [CONTINUE SHOPPING] or [LEAVE STORE]?",
+    choices: ["CONTINUE SHOPPING", "LEAVE STORE"]
+  })
+  .then(function(answer) {
+    if (answer.stayOrLeave.toUpperCase() === "CONTINUE SHOPPING") {
+      uno();
+    }
+    else {
+      console.log("Thanks for shopping with Bamazon!");
+      process.exit();
+    }
+  });
 }
-
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -17,97 +17,119 @@ var connection = mysql.createConnection({
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-    if (err) throw err;
-    // run the begin function
-    uno();
+  if (err) throw err;
+  // run the begin function
+  uno();
 });
 
 function uno() {
-	inquirer.prompt([
-		{
-			type: 'list',
-			choices: ['View Products for Sale', 'View Low Inventory', 'Modify Inventory', 'Add New Product'],
-			name: 'action',
-			message: 'Select an option to continue:'
-		}
-	]).then(function(response) {
-		if ( response.action === 'View Products for Sale' )
-			readProducts();
-        else if ( response.action === 'View Low Inventory' ) 
-            lowInventory();
-        else if ( response.action === 'Modify Inventory' )
-            modifyInventory();
-        else if ( response.action === 'Add New Product')
-            newProduct();
-	});
+inquirer.prompt([
+  {
+    type: 'list',
+    choices: ['View Products for Sale', 'View Low Inventory', 'Modify Inventory'],
+    name: 'action',
+    message: 'Select an option to continue:'
+  }
+]).then(function(response) {
+  if ( response.action === 'View Products for Sale' )
+    readProducts();
+      else if ( response.action === 'View Low Inventory' ) 
+          lowInventory();
+      else if ( response.action === 'Modify Inventory' )
+          modifyInventory();
+});
 }
 
 function readProducts() {
-    console.log("ITEM ID | PRODUCT NAME | DEPARTMENT NAME | PRICE | STOCK QUANTITY");
-    connection.query("SELECT * FROM products", function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " remaining");
-        }
-        console.log("-----------------------------------");
-        nextMove();
-    });
+  console.log("ITEM ID | PRODUCT NAME | DEPARTMENT NAME | PRICE | STOCK QUANTITY");
+  connection.query("SELECT * FROM products", function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+          console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " remaining");
+      }
+      console.log("-----------------------------------");
+      nextMove();
+  });
 }
 
 function lowInventory() {
-    console.log("ITEM ID | PRODUCT NAME | DEPARTMENT NAME | PRICE | STOCK QUANTITY");
-    connection.query("SELECT * FROM products", function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-            if (res[i].stock_quantity <= 3) {
-                console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " remaining");
-            }
-        }
-        console.log("-----------------------------------");
-        nextMove();
-    });
+  console.log("ITEM ID | PRODUCT NAME | DEPARTMENT NAME | PRICE | STOCK QUANTITY");
+  connection.query("SELECT * FROM products", function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+          if (res[i].stock_quantity <= 3) {
+              console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " remaining");
+          }
+      }
+      console.log("-----------------------------------");
+      nextMove();
+  });
 }
 
 function modifyInventory() {
-    // query the database for all items being sold
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // once you have the items, prompt the manager for inventory they'd like to modify
-      inquirer
-        .prompt([
-          {
-            name: "choice",
-            type: "rawlist",
-            choices: function() {
-              var choiceArray = [];
-              for (var i = 0; i < res.length; i++) {
-                choiceArray.push(res[i].product_name);
-              }
-              return choiceArray;
-            },
-            message: "Which product is having it's inventory modified? \n  Select by typing the item id number:"
-          },
-          {
-            name: "quantity",
-            type: "input",
-            message: "How much product would you like to add/decrease from inventory?"
-          }
-        ])
-        .then(function(answer) {
-          // get the information of the chosen item
-          var chosenItem;
-          for (var i = 0; i < res.length; i++) {
-            if (res[i].product_name === answer.choice) {
-              chosenItem = res[i];
+  // query the database for all items being sold
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // once you have the items, prompt the manager for inventory they'd like to modify
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "list",
+          pageSize: res.length + 1,
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < res.length; i++) {
+              choiceArray.push(res[i].product_name);
             }
+            return choiceArray;
+          },
+          message: "Which product is having it's inventory modified? \n  Select by typing the item id number:"
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How much product would you like to add/decrease from inventory?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        var quantity = Number(answer.quantity);
+
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].product_name === answer.choice) {
+            chosenItem = res[i];
           }
-  
-          // determine if there is enough inventory
-          if ((chosenItem.stock_quantity + parseInt(answer.quantity)) > 0) {
-            // there is enough inventory, so update db, let the user know, and start over
-            connection.query(
+        }
+
+        // determine if there is enough inventory
+        if ((chosenItem.stock_quantity + quantity) > 0) {
+          // there is enough inventory, so update db, let the user know, and start over
+          var new_amount = chosenItem.stock_quantity + quantity;
+
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: new_amount
+              },
+              {
+                item_id: chosenItem.item_id
+              }
+            ],
+            function(err, res) {
+              if (err) throw err;
+              console.log(answer.choice + " now has " + new_amount + " items in inventory.");
+              nextMove();
+            }
+          );
+        }
+        else if ((chosenItem.stock_quantity + parseInt()) <= 0) {
+          // inventory will be set to zero
+          connection.query(
               "UPDATE products SET ? WHERE ?",
               [
                 {
-                  stock_quantity: chosenItem.stock_quantity + answer.quantity
+                  stock_quantity: chosen_Item.stock_quantity + Number(quantity)
                 },
                 {
                   item_id: chosenItem.item_id
@@ -115,54 +137,35 @@ function modifyInventory() {
               ],
               function(err) {
                 if (err) throw err;
-                console.log(answer.choice + " now has " + chosenItem.stock_quantity + " items in inventory.");
+                console.log(answer.choice + " inventory has been increased");
                 nextMove();
               }
             );
-          }
-          else if ((chosenItem.stock_quantity + parseInt(answer.quantity)) <= 0) {
-            // inventory will be set to zero
-            connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                  {
-                    stock_quantity: 0
-                  },
-                  {
-                    item_id: chosenItem.item_id
-                  }
-                ],
-                function(err) {
-                  if (err) throw err;
-                  console.log(answer.choice + " inventory has been increased");
-                  nextMove();
-                }
-              );
-            nextMove();
-          }
-        });
-    });
+          nextMove();
+        }
+      });
+  });
 }
 
 function newProduct() {
-    nextMove();
+  nextMove();
 }
 
 function nextMove() {
-    inquirer
-        .prompt({
-            name:"stayOrLeave",
-            type: "rawlist",
-            message: "Would you like to [CONTINUE MANAGING] or [FINISH/EXIT]?",
-            choices: ["CONTINUE MANAGING", "FINISH/EXIT"]
-        })
-        .then(function(answer) {
-            if (answer.stayOrLeave.toUpperCase() === "CONTINUE MANAGING") {
-            uno();
-        }
-        else {
-            console.log("See you next time!");
-            process.exit();
-        }
-    });
+  inquirer
+      .prompt({
+          name:"stayOrLeave",
+          type: "list",
+          message: "Would you like to [CONTINUE MANAGING] or [FINISH/EXIT]?",
+          choices: ["CONTINUE MANAGING", "FINISH/EXIT"]
+      })
+      .then(function(answer) {
+          if (answer.stayOrLeave.toUpperCase() === "CONTINUE MANAGING") {
+          uno();
+      }
+      else {
+          console.log("See you next time!");
+          process.exit();
+      }
+  });
 }

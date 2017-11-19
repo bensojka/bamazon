ea243beef5bb619c22714c3a7b4427b349f922d7
Ben Sojka <bensojka@gmail.com>
2017-11-18 23:56:22 -0600

    readme

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# bamazon
-9/16/17
+# BAMAZON
 
-node app
+RUN ON THE COMMAND LINE 
 
 two different indexes (bamazonCustomer & bamazonManager)
 
@@ -10,3 +9,19 @@ bamazonCustomer
 
 bamazonManager
 --- add/subtract items from inventory and more features
+
+Running this application will first display all of the items available for sale.
+The app will then prompt users with two messages.
+
+The first asks them the ID of the product they would like to buy.
+The second message asks how many units of the product they would like to buy.
+
+Once the customer has placed an order, the application checks if the store has enough of the product to meet the customer's request.
+
+If not, the app will log an error phrase and then prevent the order from going through.
+
+However, if the store does have enough of the product, it will fulfill the customer's order.
+
+
+This means the SQL database gets updated to reflect the remaining quantity.
+Once the update goes through, the customer is shown the total cost of their purchase.
--- bamazon.sql
+++ bamazon.sql
@@ -14,34 +14,34 @@ PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Laptop", "Electronics", "850", "10");
+VALUES ("Laptop", "Electronics", 850.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Shoes", "Clothing", "90", "10");
+VALUES ("Shoes", "Clothing", 90.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Coffee Maker", "Kitchen", "40", "10");
+VALUES ("Coffee Maker", "Kitchen", 40.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Yoyo", "Toys & Games", "10", "10");
+VALUES ("Yoyo", "Toys & Games", 10.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Tennis Racket", "Sporting Goods", "80", "10");
+VALUES ("Tennis Racket", "Sporting Goods", 80.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("TV", "Electronics", "475", "10");
+VALUES ("TV", "Electronics", 475.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Jeans", "Clothing", "65", "10");
+VALUES ("Jeans", "Clothing", 65.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Spatula", "Kitchen", "3", "10");
+VALUES ("Spatula", "Kitchen", 3.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Fidget Spinner", "Toys & Games", "4", "10");
+VALUES ("Fidget Spinner", "Toys & Games", 4.00, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Yoga Mat", "Sporting Goods", "25", "10");
+VALUES ("Yoga Mat", "Sporting Goods", 25.00, 10);
 
 
 
